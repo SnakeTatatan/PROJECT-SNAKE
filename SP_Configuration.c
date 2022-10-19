f01d276ff1bef7589f09d06b669c18ba0999b876
Flo-1208 <116144896+Flo-1208@users.noreply.github.com>
2022-10-19 11:57:30 +0200

    Add files via upload

--- SP_Configuration.c
+++ SP_Configuration.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <time.h>
 #include "myLib.h"
 #include "SP_GESTION_JEU.h"
 #include "SP_Configuration.h"
@@ -12,46 +11,83 @@ void menu_options(ST_PARAM_JEU ParamDef)
     ST_PARAM_JEU Param=ParamDef;
     int choix1;
     int choix2;
-    printf("Couleur Stade : %d\n Couleur Snake", Param.couleur_stade, Param.couleur_snake);
-    printf("Difficulte: %d\n Dimiensions stade: %dx%d\n\n", Param.difficulte, Param.H_stade, Param.L_stade);
-    printf("voulez vous modifiez les options de jeu?\n 1.Oui\n 2.Non\n");
-    scanf("%d",choix1);
-    if(choix1==1)
+    printf("**************\n Menu Options \n**************\n\n");
+    while(choix2!=2)
     {
-        printf("1.Couleur Snake\n 2.Couleur Stade\n 3.Difficulte\n 4.Hauteur stade\n 5.Longueur Stade\n 6.Quitter");
-        scanf("%d",choix2);
-        switch (choix2)
+        printf("Couleur Stade : %d\nCouleur Snake : %d\n", Param.couleur_stade, Param.couleur_snake);
+        printf("Difficulte: %d\nDimiensions stade: %dx%d\n\n", Param.difficulte, Param.H_stade, Param.L_stade);
+        printf("voulez vous modifiez les options de jeu?\n 1.Oui\n 2.Non\n\n");
+        scanf("%d",&choix1);
+        if(choix1==1)
         {
-            case 1:
-                printf("modifiez l'option");
-                scanf("%d", Param.couleur_snake);
-                break;
-            case 2:
-                printf("modifiez l'option");
-                scanf("%d", Param.couleur_stade);
-                break;
-            case 3:
-                printf("modifiez l'option");
-                scanf("%d", Param.difficulte);
-                break;
-            case 4 :
-                printf("modifiez l'option");
-                scanf("%d", Param.H_stade);
-                break;
-            case 5 :
-                printf("modifiez l'option");
-                scanf("%d", Param.L_stade);
-                break;
-            default :
-                printf("quitter");
-                break;
+            printf("\n\n 1.Couleur Stade\n 2.Couleur Snake\n 3.Difficulte\n 4.Hauteur stade\n 5.Longueur Stade\n 6.Quitter\n\n");
+            scanf("%d", &choix2);
+            switch (choix2)
+            {
+                case 1:
+                    printf("\n modifiez l'option\n");
+                    scanf("%d", &Param.couleur_stade);
+                    break;
+                case 2:
+                    printf("\n modifiez l'option\n");
+                    scanf("%d", &Param.couleur_snake);
+                    break;
+                case 3:
+                    printf("\n modifiez l'option\n");
+                    scanf("%d", &Param.difficulte);
+                    break;
+                case 4 :
+                    printf("\n modifiez l'option\n");
+                    scanf("%d", &Param.H_stade);
+                    break;
+                case 5 :
+                    printf("\n modifiez l'option\n");
+                    scanf("%d", &Param.L_stade);
+                    break;
+                case 6 :
+                    SP_Titre();
+                    SP_menuppl();
+                    break;
+            }
         }
-        printf("Couleur Stade : %d\n Couleur Snake", Param.couleur_stade, Param.couleur_snake);
-        printf("Difficulte: %d\n Dimiensions stade: %dx%d\n\n", Param.difficulte, Param.H_stade, Param.L_stade);
+        else if (choix1==2)
+        {
+            printf("pas de modif des options, retour à l'acceuil");
+            SP_Titre();
+            SP_menuppl();
+        }
+        else
+        {
+            printf("erreur");
+        }
     }
-    else
-    {
-        printf("pas de modif des options");
-    }
+}
 
+void SP_Titre()
+{
+    setColor(RED);
+    gotoxy(30,5);
+    printf("**************************************\n");
+    gotoxy(40,6);
+    printf("BIENVENUE DANS SNAKE\n");
+    gotoxy(30,7);
+    printf("**************************************\n");
+    hidecursor();
+}
+
+void SP_menuppl()
+{
+        gotoxy(40,10);
+        setColor(GREEN);
+        printf("%c Jouer\n",254);
+        setColor(YELLOW);
+        gotoxy(40,12);
+        printf("%c Options",254);
+        setColor(BLUE);
+        gotoxy(40,14);
+        printf("%c Scores",254);
+        gotoxy(40,16);
+        setColor(RED);
+        printf("%c Quitter\n\n",254);
+        setColor(WHITE);
 }
--- main.c
+++ main.c
@@ -1,40 +1,31 @@
 #include <stdio.h>
 #include "myLib.h"
 #include "SP_GESTION_JEU.h"
-#include "Mes_SP.h"
+#include "SP_Configuration.h"
 #include "MesTypes.h"
 
 int main()
 {
-    setColor(RED);
-    gotoxy(30,5);
-    printf("**************************************\n");
-    gotoxy(40,6);
-    printf("BIENVENUE DANS SNAKE\n");
-    gotoxy(30,7);
-    printf("**************************************\n");
+    /*initialisation des paramètres par défaut*/
+    ST_PARAM_JEU ParamDefaut;
+    ParamDefaut.couleur_snake=BLUE;
+    ParamDefaut.couleur_stade=YELLOW;
+    ParamDefaut.difficulte=1;
+    ParamDefaut.H_stade=5;
+    ParamDefaut.L_stade=5;
+    /*mise en page : SP_Titre*/
+    SP_Titre();
+    /*choix action*/
     char choix[10];
-    hidecursor();
     while (strcmp(choix,"quitter") || strcmp(choix,"quitter") == 1 )
     {
-        printf("bonjouuuuuuur %s",choix);
-        gotoxy(40,10);
-        setColor(GREEN);
-        printf("%c Jouer\n",254);
-        setColor(YELLOW);
-        gotoxy(40,12);
-        printf("%c Options",254);
-        setColor(BLUE);
-        gotoxy(40,14);
-        printf("%c Scores",254);
-        gotoxy(40,16);
-        setColor(RED);
-        printf("%c Quitter\n\n",254);
-        setColor(WHITE);
+        /*menu principal : SP_menuppl*/
+        SP_menuppl();
+        /*choix joueur*/
         scanf("%s",choix);
-        if (strcmp(choix,"Options") strcmp(choix,"options"))
+        if (strcmp(choix,"Options") || strcmp(choix,"options"))
         {
-
+            menu_options(ParamDefaut);
         }
         else if (strcmp (choix,"Jouer") || strcmp(choix,"jouer"))
         {
@@ -47,11 +38,9 @@ int main()
         else
         {
             printf("Erreur de saisie, veuillez réessayer\n");
-
         }
     }
 
     return 0;
 }
-
 
